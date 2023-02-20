267e4bb76663a619bc4caf5c924ea1cf61516103
User <user@its.me>
2023-02-20 12:23:27 +0100

    Initial commit

---
+++ src/main/java/online/javafun/thymform/ThymeleafFormsApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.thymform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafFormsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/javafun/thymform/ThymeleafFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.thymform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFormsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
